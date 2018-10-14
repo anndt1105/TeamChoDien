3d864dad28cdc23122916a68c6ede41f388bda63
Minh Thùy Trần <43169818+minhthuytran@users.noreply.github.com>
2018-10-14 16:26:18 +0700

    edit ConstantVariable

--- WSM_Automation3/src/wsm/teamChoDien/Utility/ConstantVariable.java
+++ WSM_Automation3/src/wsm/teamChoDien/Utility/ConstantVariable.java
@@ -1,13 +1,13 @@
 package wsm.teamChoDien.Utility;
 
 public class ConstantVariable {
-	public static final String URL = "https://wsm.framgia.vn";
+	public static final String URL = "https://edev.framgia.vn/";
 	 
-    public static final String USERNAME = "doan.ngoc.vu@framgia.com";
+    public static final String USERNAME = "nguyen.duc.truong.an@framgia.com.edev";
     
     public static final String USERNAME_NOT_EXIT = "doan.ngoc.vu123@framgia.com";
 
-    public static final String PASSWORD = "ngocvudoan8292";
+    public static final String PASSWORD = "12345678";
     
     public static final String LOGIN_FAILED_MESSAGE = "Invalid email or password.";
     
