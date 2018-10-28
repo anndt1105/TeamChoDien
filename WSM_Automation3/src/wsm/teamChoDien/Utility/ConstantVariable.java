2047d7306c1695db012f5e87cc2313e6dafc3749
Hoa DC <dang.cao.hoa@framgia.com>
2018-10-28 13:46:46 +0700

    Hoa add file ConstantVariable

--- WSM_Automation3/src/wsm/teamChoDien/Utility/ConstantVariable.java
+++ WSM_Automation3/src/wsm/teamChoDien/Utility/ConstantVariable.java
@@ -1,11 +1,13 @@
 package wsm.teamChoDien.Utility;
 
 public class ConstantVariable {
-	public static final String URL = "https://wsm.framgia.vn";
+	public static final String URL = "https://edev.framgia.vn";
 	 
     public static final String USERNAME = "doan.ngoc.vu@framgia.com";
     
     public static final String USERNAME_NOT_EXIT = "doan.ngoc.vu123@framgia.com";
+    
+    public static final String USERNAME_INVALID = "doan.ngoc.vu123@";
 
     public static final String PASSWORD = "ngocvudoan8292";
     
@@ -13,5 +15,14 @@ public class ConstantVariable {
     
     public static final String USERNAME_BLANK_MESSAGE = "Email is required";
     
+    public static final String USERNAME_INVALID_MESSAGE = "Please enter a valid email address";
+    
+    public static final String USERNAME_NOT_EXIST_MESSAGE = "Email not found";
+    
+    public static final String SEND_SUCCESSFULL_MESSAGE =
+    		"You will receive an email with instructions on how to reset your password in a few minutes.";
+    
     public static final String PASSWORD_BLANK_MESSAGE = "Password is required";
+    
+    public static final String FORGOTPASSWORD_TITLE = "FORGOT PASSWORD";
 }
