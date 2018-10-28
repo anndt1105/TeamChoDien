4ace6cd718f9173fcd8fec59accf7979b8ce1a3c
Minh Thùy Trần <43169818+minhthuytran@users.noreply.github.com>
2018-10-28 15:37:26 +0700

    edit action Request OT action

--- WSM_Automation3/src/wsm/teamChoDien/Utility/ConstantVariable.java
+++ WSM_Automation3/src/wsm/teamChoDien/Utility/ConstantVariable.java
@@ -18,4 +18,20 @@ public class ConstantVariable {
     public static final String USERNAME_BLANK_MESSAGE = "Email is required";
     
     public static final String PASSWORD_BLANK_MESSAGE = "Password is required";
+    
+    //Personal request
+    //Branch 4=Da Nang Office
+    public static final String BRANCH_VALID = "4";
+    
+    //Group 370=Div_test fgas/Div test _ sec 1/Group 1/Team 2/Team member 123
+    public static final String GROUP_VALID = "370";
+    public static final String PROJECT_VALID = "Beautiful Company";
+    public static final String RESON_OT = "Because I love my Customer";
+    // Request OT date
+    public static String REQUEST_OT_YEAR_MONTH = "";
+    public static String REQUEST_OT_DATE = "";
+    
+    //Message create unsuccessuly
+    public static final String REQUEST_OT_DATE_IN_PAST_MESS = "Timekeeping data of " +REQUEST_OT_DATE+" is not available, you can not access for this month!";
+    
 }
